864aa2596db3a2f737a5258d42d4abe77cd47bc8
Clément Verger <clemeverger@gmail.com>
2022-09-21 16:32:41 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Point, useTexture } from '@react-three/drei';
-import React, { useRef, useMemo, useState } from 'react'
+import { OrbitControls, Point, useTexture } from '@react-three/drei';
+import React, { useRef, useMemo, useState, useEffect } from 'react'
 import vertexShader from './shaders/vertexShader.glsl'
 import fragmentShader from './shaders/fragmentShader.glsl'
 
@@ -22,7 +22,6 @@ const font = new FontLoader().parse(myFont);
 
 const App = () => {
 
-  console.log("Qu'en pensez vous SouthVibes ?")
   const c = useTexture('planet-c++.png');
   const css = useTexture('planet-css.png');
   const html = useTexture('planet-html.png');
@@ -47,18 +46,21 @@ const App = () => {
 
   const [focus, setFocus] = useState(false);
 
+  const orbitControls = useRef();
+
   return (
     <>
       <group ref={groupRef}>
-        <Planete texture={c} position={[-6, 3, 0]} focus={focus} setFocus={setFocus}></Planete>
-        <Planete texture={css} position={[-2, 3, 0]} focus={focus} setFocus={setFocus}></Planete>
-        <Planete texture={html} position={[2, 3, 0]} focus={focus} setFocus={setFocus}></Planete>
-        <Planete texture={js} position={[6, 3, 0]} focus={focus} setFocus={setFocus}></Planete>
+        <OrbitControls ref={orbitControls} />
+        <Planete texture={c} position={[-6, 3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
+        <Planete texture={css} position={[-2, 3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
+        <Planete texture={html} position={[2, 3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
+        <Planete texture={js} position={[6, 3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
 
-        <Planete texture={php} position={[-6, -3, 0]} focus={focus} setFocus={setFocus}></Planete>
-        <Planete texture={python} position={[-2, -3, 0]} focus={focus} setFocus={setFocus}></Planete>
-        <Planete texture={ruby} position={[2, -3, 0]} focus={focus} setFocus={setFocus}></Planete>
-        <Planete texture={vuejs} position={[6, -3, 0]} focus={focus} setFocus={setFocus}></Planete>
+        <Planete texture={php} position={[-6, -3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
+        <Planete texture={python} position={[-2, -3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
+        <Planete texture={ruby} position={[2, -3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
+        <Planete texture={vuejs} position={[6, -3, 0]} focus={focus} setFocus={setFocus} orbitControls={orbitControls}></Planete>
         <points>
           <bufferGeometry>
             <bufferAttribute attach={"attributes-position"} {...points} />
@@ -75,21 +77,27 @@ const App = () => {
 
 export default App
 
-const Planete = ({ texture, position, focus, setFocus }) => {
+const Planete = ({ texture, position, focus, setFocus, orbitControls }) => {
   const planet = useRef();
+
   useFrame(({ camera }) => {
-    if (!focus) {
-      planet.current.rotation.y += 0.0025;
-      planet.current.rotation.x += 0.0025;
-      planet.current.rotation.z += 0.0025;
-    }
-    else {
-      camera.position.z -= 0.1;
+    if (planet.current === focus) {
+      if (!focus) {
+        planet.current.rotation.y += 0.0025;
+        planet.current.rotation.x += 0.0025;
+        planet.current.rotation.z += 0.0025;
+      }
+      else {
+        gsap.to(camera.position, { z: 15, duration: 3, ease: 'easeInOut', });
+        camera.lookAt(planet.current.position);
+      }
     }
   })
+
+  if (focus && planet.current !== focus) return <></>
   return (
     <>
-      <mesh onClick={() => setFocus(true)} ref={planet} position={position}>
+      <mesh onClick={() => setFocus(planet.current)} ref={planet} position={position}>
         <sphereGeometry />
         <shaderMaterial
           vertexShader={vertexShader}
--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Suspense fallback={null}>
           <App></App>
         </Suspense>
-        <OrbitControls />
       </Canvas>
     </>
   </React.StrictMode>
