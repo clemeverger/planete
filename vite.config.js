9cc8e4507510628e5827598b3f021fc495aa47e8
ANCELIN DOUGLAS <ancelindouglas@gmail.com>
2022-09-23 08:32:21 +0200

    Save new Version

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginString from 'vite-plugin-string'
@@ -9,3 +17,4 @@ export default defineConfig({
     vitePluginString.default()
   ]
 })
+
